ab3265a82c12df912ff7dced534684c36dbe3a2b
Jake Wags <jakewagoneredu@gmail.com>
2022-08-24 16:53:15 -0600

    Added http to scan

--- scan.js
+++ scan.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env nodejs
 'use strict';
 
 require('dotenv').config();
 const snoowrap = require('snoowrap');
+const http = require('http');
 
 function scrapeSubreddit() {
     const r = new snoowrap({
@@ -17,11 +19,23 @@ function scrapeSubreddit() {
     // user provides itemType in command line
     let itemType = process.argv[2];
     let postLimit = parseInt(process.argv[3]);
+    let retText = [];
+
     let posts = r.getSubreddit("buildapcsales").getNew({limit:postLimit}).map((post) => {
         if (post.link_flair_text == itemType) {
-            console.log(post.title);
+            retText.append(post.title);
         }
     });
+
+    return retText;
 }
 
-scrapeSubreddit();
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    let disString = "";
+    
+    scrapeSubreddit().forEach((item) => {disString += item + "\n"});
+    
+    response.end(disString);
+}).listen(8080);
+

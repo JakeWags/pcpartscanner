71fae53a132260b524642e91ff210096941da1a8
Jake Wags <jakewagoneredu@gmail.com>
2022-08-25 14:24:11 -0600

    Added subreddit scraping (plain text output)

--- scan.js
+++ scan.js
@@ -3,8 +3,7 @@
 
 require('dotenv').config();
 const snoowrap = require('snoowrap');
-// const http = require('http');
-// const fs = require('fs');
+
 const express = require('express');
 const { body,validationResult } = require('express-validator');
 const path = require('path');
@@ -17,7 +16,7 @@ app.use(express.urlencoded({
 }));
 
 const r = new snoowrap({
-    userAgent: 'Scans /r/buildapcsales for part',
+    userAgent: 'Scans /r/buildapcsales for parts',
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     refreshToken: process.env.REFRESH_TOKEN
@@ -25,67 +24,46 @@ const r = new snoowrap({
 
 const searchType = process.env.SEARCHTYPE;
 
-async function scrapeSubreddit() {
+async function scrapeSubreddit(type, amount) {
     // user provides itemType in command line
-    let itemType = process.argv[2];
-    let postLimit = parseInt(process.argv[3]);
     let retText = [];
+    console.log(`Scraping /r/buildapcsales most recent ${amount} posts for ${type}s...`);
 
-    let posts = await r.getSubreddit("buildapcsales").getNew({limit:postLimit}).map((post) => {
-        if (post.link_flair_text == itemType) {
+    let posts = await r.getSubreddit("buildapcsales").getNew({limit:amount}).map((post) => {
+        if (post.link_flair_text == type) {
             retText.push(post);
         }
     });
 
+
     return Promise.resolve(retText);
 }
-
-// http.createServer(async function (request, response) {
-//     // response.writeHead(200, {'Content-Type': 'text/plain'});
-//     // let disString = "";
-    
-//     // let r = await scrapeSubreddit().then((p) => {
-//     //     p.forEach(post => {disString += post.title + " : " + post.url + "\n";})
-//     // });
-
-//     // response.end(disString);
-      
-//     response.writeHead(200, {
-//         'Content-Type': 'type/plain'
-//     });
-//     fs.readFile('./scan.html', null, function (error, data) {
-//         if (error) {
-//             response.writeHead(404);
-//             response.write("Error: File not found.");
-//         }
-//         else {
-//             response.write(data);
-//         }
-//         response.end();
-//     })
-//  }).listen(8080);
-
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'scan.html'));
 });
 
-app.post('/submit-scan', body("type").not().contains("Select a part type"), body("amount").isLength({max:3}), (req, res) => {
+app.post('/submit-scan', body("type").not().contains("Select a part type"), body("amount").isLength({max:3}), async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
     const select = req.body.type;
-    const amount = req.body.amount;
-    console.log(select);
-    console.log(amount);
+    const amount = parseInt(req.body.amount);
+
+    let displayString = "";
     
+    let r = await scrapeSubreddit(select, amount).then((p) => {
+        p.forEach(post => {displayString += post.title + " : " + post.url + "\n";})
+    });
 
-    // do snoowrap api calls here and populate /submit-scan with editted html?
-
-    res.end(select + " " + amount);
-})
+    if (disString.length == 0) {
+        res.end("No results found.");
+    } else {
+        res.end(displayString);
+    }
+});
 
 
 app.listen(port);
